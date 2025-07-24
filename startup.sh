7aa31dae33571aedd50a2f3bfdb88ea5f0c45171
Ian Hanson <ian.hanson@rbkc.gov.uk>
2025-07-24 14:01:02 +0100

    remove certs from startup

--- startup.sh
+++ startup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# # Copy CA cert to trusted store  
-# cp /home/site/wwwroot/rbkcrootca.cer /usr/local/share/ca-certificates/rbkcroot.cer 
-# cp /home/site/wwwroot/wccrootca.cer /usr/local/share/ca-certificates/wccrootca.cer 
-  
-# # Update CA certificates store  
-# update-ca-certificates  
-
 uvicorn backend.api:app --host 0.0.0.0 --port 8000
+
