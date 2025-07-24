d7d6ff06f3e9ebc089cfaae450c23169233366cd
Ian Hanson <ian.hanson@rbkc.gov.uk>
2025-07-24 11:09:54 +0100

    removed certs from startup

--- startup.sh
+++ startup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Copy CA cert to trusted store  
-cp /home/site/wwwroot/rbkcrootca.cer /usr/local/share/ca-certificates/rbkcroot.cer 
-cp /home/site/wwwroot/wccrootca.cer /usr/local/share/ca-certificates/wccrootca.cer 
+# # Copy CA cert to trusted store  
+# cp /home/site/wwwroot/rbkcrootca.cer /usr/local/share/ca-certificates/rbkcroot.cer 
+# cp /home/site/wwwroot/wccrootca.cer /usr/local/share/ca-certificates/wccrootca.cer 
   
-# Update CA certificates store  
-update-ca-certificates  
+# # Update CA certificates store  
+# update-ca-certificates  
 
 uvicorn backend.api:app --host 0.0.0.0 --port 8000
-# gunicorn backend.api:app --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
